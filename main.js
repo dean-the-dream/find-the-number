4b50ce80b9918d33da81c1ad1c6a855da2f31fb2
Dean-The-Dream <lawrence_dean@icloud.com>
2022-09-15 19:54:27 -0400

    fix: update high and low guess functions

--- main.js
+++ main.js
@@ -1,42 +1,65 @@
 const randomNumber = Math.floor(Math.random() * 100) + 1;
-console.log(randomNumber)
 const mainSection = document.querySelector("#main");
 let userGuess = document.querySelector("input");
-let userGuessInt
+let userGuessInt;
 let lowNum = 1;
 let highNum = 100;
 let guesses = 5;
-let attempts = document.querySelector("#attempts");  //.innerHTML = `<p>You have ${guesses} guesses left!</p>`
+let attempts = document.querySelector("#attempts");
+let statusParent = document.querySelector("status");
+let statusChild;  //.innerHTML = `<p>You have ${guesses} guesses left!</p>`
 function checkGuess(e) {
     
     e.preventDefault();
     if(e.target.id == "guess"){
         console.log("Checking the guess")
         userGuessInt = Number(userGuess.value);
-        console.log(userGuessInt)
         userGuess.value = "";
-        (Number(x) == randomNumber) ? guessRight() : guessWrong(); 
+        if(userGuessInt == randomNumber){
+            guessRight()
+        } else {
+            guessWrong()
+        }
+        // (userGuessInt == randomNumber) ? guessRight() : guessWrong(); 
     }
     
 }
 
 function guessRight() {
     console.log("The Guess Is Correct")
-    
+    statusChild = 
 
 }
 
 function guessWrong() {
     console.log("The Guess Is Incorrect")
 
+    if(userGuessInt > randomNumber){
+        guessHigh();
+    } else {
+        guessLow();
+    }
+
 }
 
 function guessHigh() {
     console.log("The Guess Is High")
+    reduceGuess();
+    highNum = userGuess;
 }
 
 function guessLow() {
     console.log("The Guess Is Low")
+    reduceGuess();
+    lowNum = userGuess;
+}
+
+function reduceGuess() {
+    guesses -= 1;
+}
+
+function displayResults () {
+
 }
 function tryAgain(){
     
