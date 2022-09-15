6da940d5c5541cf8ba12372665c6b7300537e5ae
Dean-The-Dream <lawrence_dean@icloud.com>
2022-09-15 04:31:22 -0400

    update: add guessing funtions

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
 const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber)
 const mainSection = document.querySelector("#main");
-let userguess = document.querySelector("input");
+let userGuess = document.querySelector("input");
+let userGuessInt
 let lowNum = 1;
 let highNum = 100;
 let guesses = 5;
 let attempts = document.querySelector("#attempts");  //.innerHTML = `<p>You have ${guesses} guesses left!</p>`
 function checkGuess(e) {
+    
     e.preventDefault();
     if(e.target.id == "guess"){
-        
-        let x = userguess.value;
-        userguess.innerText = "";
+        console.log("Checking the guess")
+        userGuessInt = Number(userGuess.value);
+        console.log(userGuessInt)
+        userGuess.value = "";
         (Number(x) == randomNumber) ? guessRight() : guessWrong(); 
     }
     
 }
 
 function guessRight() {
+    console.log("The Guess Is Correct")
+    
 
 }
 
 function guessWrong() {
+    console.log("The Guess Is Incorrect")
 
 }
 
 function guessHigh() {
-
+    console.log("The Guess Is High")
 }
 
 function guessLow() {
-
+    console.log("The Guess Is Low")
 }
 function tryAgain(){
     
